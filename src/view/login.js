42bf3f6e2c10816a5765829be47c8553094a8f85
jumorap <jumorap@unal.edu.co>
2021-04-22 13:43:27 -0500

    From the view design in Figma, designed by the Front team, was created the view of login.js that works too with login.css

--- src/view/login.js
+++ src/view/login.js
@@ -5,6 +5,8 @@ import React, { Component } from "react";
 import Button from '@material-ui/core/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import faGoogle from '@fortawesome/fontawesome-free-brands/faGoogle';
+import '../css/login.css';
+import principalNameLogo from '../assets/principal-slogan-logo.png';
 
 class Login extends Component {
     render() {
@@ -14,25 +16,52 @@ class Login extends Component {
             signInWithGoogle,
         } = this.props;
         return (
-            <div>
-                {
-                    user
-                        ? <p>Hello, {user.displayName}</p>
-                        : <p>Please, sign in</p>
-                }
-                {
-                    user
-                        ? <Button onClick={signOut}
-                                  variant="contained"
-                                  color="primary"
-                                  size="large"
-                                  startIcon={<FontAwesomeIcon icon={faGoogle} />}>Sign out</Button>
-                        : <Button onClick={signInWithGoogle}
-                                  variant="contained"
-                                  color="primary"
-                                  size="large"
-                                  startIcon={<FontAwesomeIcon icon={faGoogle} />}>Sign in with Google</Button>
-                }
+            <div className={'div-general'}>
+                <div className={['div-left', 'div-half-screen'].join(' ')} />
+                <div className={['div-right', 'div-half-screen'].join(' ')}>
+                    <div className={'container-div-right'}>
+                        {
+                            user
+                                ? <p>Hello, {user.displayName}</p>
+                                : <p><img src={principalNameLogo} className={'slogan-img'} alt={'logo'}/></p>
+                        }
+                        {
+                            user
+                                ? <Button onClick={signOut}
+                                          variant="contained"
+                                          color="primary"
+                                          size="large"
+                                          startIcon={<FontAwesomeIcon icon={faGoogle} />}>Sign out</Button>
+                                : <Button onClick={signInWithGoogle}
+                                          className={'google-sign'}
+                                          style={{
+                                              background: 'linear-gradient(45deg, #FFF 30%, #FFF 90%)',
+                                              borderRadius: 30,
+                                              border: 0,
+                                              color: '#000',
+                                              height: 48,
+                                              padding: '0 30px',
+                                              boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
+                                          }}
+                                          variant="contained"
+                                          color="primary"
+                                          size="large"
+                                          startIcon={<FontAwesomeIcon icon={faGoogle} />}>Sign in with Google</Button>
+                        }
+                        {
+                            user
+                                ? <hr />
+                                : <center>
+                                    <span className={'legal'}>
+                                        AL INGRESAR ESTÁS ACEPTANDO NUESTROS
+                                        <a className={'politics'} href={'https://www.github.com'}>
+                                            TÉRMINOS, CONDICIONES Y POLÍTICAS DE PRIVACIDAD
+                                        </a>
+                                    </span>
+                                </center>
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
