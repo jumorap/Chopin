266f1979261168bc53673613420175b51efec9cb
Johan <jorodriguezal@unal.edu.co>
2021-04-23 15:50:08 -0500

    CreateProfesor

--- src/model/Profesores.js
+++ src/model/Profesores.js
@@ -1,9 +1,16 @@
-class Profesroes{
+class Profesores{
+    static _DBdisplayCollection = db.collection("MATERIAS_DISPLAY")
     /**
      * Crea El documento profesor en la coleccion profesores
      * @param  {String} nombre Nombre de el profesor que se va a crear
      */
     static CreateProfesor = (nombre)=>{
-        
+        Profesores._DBdisplayCollection.add({
+            nombre: nombre
+        }).then(function(err){
+            console.log("Profesor agregado exitosamente")
+        }).catch(function(err){
+            console.log("Error with CreateProfesor")
+        })
     }
 }
