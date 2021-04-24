51bf558175bbe09a3789981bd52bd293736ba944
Proft1827 <srodriguezt@unal.edu.co>
2021-04-24 14:31:52 -0500

    First NavBar version

--- src/view/uploadPage/Navbar.js
+++ src/view/uploadPage/Navbar.js
@@ -1,9 +1,35 @@
 import React from "react";
+import './css/NBar.css';
+import logo from '../../assets/react_logo.png'
+import uicon from '../../assets/User.png'
+
 
 const Navbar = () => {
   return (
-    <div className="navigation border">
-      <div className="">Navbar component</div>
+    <div className = "contain">
+    <button>
+
+        <img src={logo}  style = {{ 
+          position: "absolute",
+          width: "303px",
+          height: "auto",
+          marginLeft: "10px",
+          marginTop: "15px",
+        }}
+        onClick={""}
+        />
+
+      </button>
+    
+      <button>
+        <img src={uicon}  style = {{ 
+          position: "absolute",
+          width: "200px",
+          height: "auto",
+          marginLeft: "1610px",
+          marginTop: "0px",
+          }}/>
+        </button>
     </div>
   );
 };
