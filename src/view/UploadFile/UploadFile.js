95c3bbb1ccb9ffa74de5ccf8a74d93e8bde2d00a
Jahel Leon <jleonro@unal.edu.co>
2021-05-09 21:24:15 -0500

    upload file finished

---
+++ src/view/UploadFile/UploadFile.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import Modal from '@material-ui/core/Modal';
+import AddIcon from '@material-ui/icons/Add';
+import { IconButton } from '@material-ui/core';
+import "../css/uploadFile.css"
+import UploadForm from './UploadForm';
+
+const UploadFile = () => {
+    const styles = {
+        openButton: {            
+            position:'fixed',
+            bottom: "3rem",
+            right: "4rem",
+            backgroundColor: "#A01111"                    
+        },        
+        openIcon: {color : "white"}
+    }
+
+    
+        
+    const [open, setopen] = useState(false)
+
+    const handleOpen = () => {
+        setopen(true)
+    }
+
+    const handleClose = () =>{
+        setopen(false)
+    }
+
+    
+    return (
+        <div >
+            <Modal
+                open = {open}
+                onClose = {handleClose} 
+                className = "ModalWindow"               
+            >
+                <UploadForm/>
+            </Modal>
+            
+            <IconButton style = {styles.openButton} className = "openButton" onClick = {handleOpen}> 
+                <AddIcon fontSize = "large"  className = "openIcon"/>
+            </IconButton>
+        </div>
+    )
+}
+
+export default UploadFile
