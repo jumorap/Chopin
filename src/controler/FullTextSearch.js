29746f10e1cfa7a4f6e4d930b9f37a7ec1a99fca
Jahel Leon <jleonro@unal.edu.co>
2021-05-12 13:14:51 -0500

    Search bar fixed, now full text search is initialized in the context provider
    (cherry picked from commit 43c627c15e867ebda2a8a4167206ca97c88691f8)
    
    # Conflicts:
    #	src/controler/FullTextSearch.js
    #	src/model/Materias.js
    #	src/view/ContextProvider.js
    #	src/view/components/Searcher.js

--- src/controler/FullTextSearch.js
+++ src/controler/FullTextSearch.js
@@ -1,47 +1,50 @@
-import elasticlunr from "elasticlunr";
+import elasticlunr from "elasticlunr"
 
-class FullTextSearch {
-  /**
-   * Crea un objeto para realizar la busqueda de texto en este
-   * @param  {Obj} data Objeto que contiene los datos en la forma ID:data
-   */
-  constructor(data) {
-    this.data = data;
-    this.index = elasticlunr();
-    elasticlunr.clearStopWords();
-    this.idMateriaMap = new Map();
-    this._createReverseIndex(this.data);
-  }
+class FullTextSearch{
+    /**
+     * Crea un objeto para realizar la busqueda de texto en este
+     * @param  {Obj} data Objeto que contiene los datos en la forma ID:data    
+     */
+    constructor(data) {
+        this.data = data
+        this.index = elasticlunr();
+        elasticlunr.clearStopWords();          
+        this.idMateriaMap = new Map()
+        this._createReverseIndex(this.data)          
+    }
 
-  /**
-   * Crea los indices para la busqueda de texto completa con la libreria elasticlunr
-   * @param  {Obj} data objeto que contiene los datos en la forma ID:data
-   */
-  _createReverseIndex(data) {
-    this.index.addField("materia");
-    this.index.setRef("id");
-    data.map((materia) => {
-      this.index.addDoc(materia);
-      this.idMateriaMap.set(materia.id, materia.materia);
-    });
-  }
+    /**
+     * Crea los indices para la busqueda de texto completa con la libreria elasticlunr
+     * @param  {Obj} data objeto que contiene los datos en la forma ID:data    
+     */
+    _createReverseIndex(data) {
+        this.index.addField("materia")
+        this.index.setRef("id")        
+        data.map(materia=>{
+            this.index.addDoc(materia)            
+            this.idMateriaMap.set(materia.id, materia.materia)            
+        })        
+        
+    }
 
-  /**
-   * Busca dentro de los datos disponibles la string search y retorna un arreglo con los resultados dado un String "search" que se va a buscar
-   * @param  {String} search texto que desea buscar
-   * @return {Array}      arreglo que contiene los resultados de la busqueda en la fomra {id, data}
-   */
-  queryData(search) {
-    let serch_results = this.index.search(search, {
-      expand: true,
-    });
-    console.log(`Search reuslts${serch_results}`);
-    let results = [];
-    serch_results.forEach((obj) => {
-      results.push({ id: obj.ref, data: this.idMateriaMap.get(obj.ref) });
-    });
-    return results;
-  }
+    
+    /**
+     * Busca dentro de los datos disponibles la string search y retorna un arreglo con los resultados dado un String "search" que se va a buscar
+     * @param  {String} search texto que desea buscar
+     * @return {Array}      arreglo que contiene los resultados de la busqueda en la fomra {id, data}
+     */
+    queryData(search) {
+        let serch_results = this.index.search(search, {
+            expand: true
+        })        
+        console.log(`Search reuslts${serch_results}`)
+        let results = []        
+        serch_results.forEach((obj) => {
+            results.push({id: obj.ref, data: this.idMateriaMap.get(obj.ref)})
+        })
+        return results
+    }
+    
 }
 
-export default FullTextSearch;
+export default FullTextSearch
--- src/view/components/Searcher.js
+++ src/view/components/Searcher.js
@@ -1,81 +1,69 @@
-import React, { useEffect, useRef, useState } from "react";
-import SearchItem from "./SearchItem";
-import "../css/searcher.css";
+import React, { useEffect, useRef, useState } from 'react'
+import SearchItem from './SearchItem'
+import "../css/searcher.css"
 
 //icons
-import SearchIcon from "@material-ui/icons/Search";
-import ClearIcon from "@material-ui/icons/Clear";
-import IconButton from "@material-ui/core/IconButton";
-import { useFullTextSearch, useMaterias } from "../ContextProvider";
+import SearchIcon from '@material-ui/icons/Search';
+import ClearIcon from '@material-ui/icons/Clear';
+import IconButton from '@material-ui/core/IconButton';
+import FullTextSearch from '../../controler/FullTextSearch';
+import {useFullTextSearch, useMaterias} from '../ContextProvider';
+
 
 function Searcher() {
-  /**Texto de la barra del buscador */
-  const [searchText, setsearchText] = useState("");
+    
+    /**Texto de la barra del buscador */
+    const [searchText, setsearchText] = useState("")
+    
+    /**Usado para  mostrar las materias dado una busqueda*/
+    const [searchResults, setsearchResults] = useState([])
 
-  /**Usado para  mostrar las materias dado una busqueda*/
-  const [searchResults, setsearchResults] = useState([]);
+    const firstRender = useRef(true)
+    
+    /**Traemos la lista de materias de el context */
+    const listaMaterias = useMaterias()        
 
-  const firstRender = useRef(true);
-
-  /**Traemos la lista de materias de el context */
-  const listaMaterias = useMaterias();
-
-  /**Traemos el full textSearch del context*/
-  const searcherEngine = useFullTextSearch();
-
-  /**Modifica el texto de input y los resultados que este arroja */
-  const handleInputTextChange = (e) => {
-    setsearchText(e.target.value);
-  };
-
-  /**side effect que actualiza la searchResults cada vez que cambia la input */
-  useEffect(() => {
-    if (firstRender.current === true) {
-      firstRender.current = false;
-    } else {
-      setsearchResults(searcherEngine.queryData(searchText));
+    /**Traemos el full textSearch del context*/
+    const searcherEngine = useFullTextSearch()
+    
+                
+    /**Modifica el texto de input y los resultados que este arroja */
+    const  handleInputTextChange =  (e) =>{
+        setsearchText(e.target.value)                        
     }
-  }, [searchText, searcherEngine]);
+    
+    /**side effect que actualiza la searchResults cada vez que cambia la input */
+    useEffect(() => {        
+        if(firstRender.current === true){
+            
+            firstRender.current = false
+        }else{
+            setsearchResults(searcherEngine.queryData(searchText))                            
+        }
+    }, [searchText])
+    
+    /**activated when the close button is pressed */
+    const handleCloseButton = () =>{
+        setsearchText("")
+    }
 
-  /**activated when the close button is pressed */
-  const handleCloseButton = () => {
-    setsearchText("");
-  };
+    return (        
+        <div className = "searcher-container" >
+            <div className="searcher-container-input ">
+                <input type="text" name="search" id="search" className="searcher-input" placeholder="Escribe el nombre de la materia" value={searchText} onChange={handleInputTextChange} autocomplete="off"/>
+                <div className="searcher-icons faic">
+                    <IconButton onClick={handleCloseButton}>
+                        <ClearIcon className="icon"/>
+                    </IconButton>
+                    <SearchIcon className="icon"/>
+                </div>
+            </div>
 
-  return (
-    <div className="searcher-container">
-      <div className="searcher-container-input ">
-        <input
-          type="text"
-          name="search"
-          id="search"
-          className="searcher-input"
-          placeholder="Escribe el nombre de la materia"
-          value={searchText}
-          onChange={handleInputTextChange}
-          autocomplete="off"
-        />
-        <div className="faic">
-          <IconButton
-            onClick={handleCloseButton}
-            style={{
-              margin: 0,
-              padding: 0,
-              marginRight: "5px",
-            }}
-          >
-            <ClearIcon className="icon" />
-          </IconButton>
-          <SearchIcon className="icon" />
-        </div>
-      </div>
+            {/* Diplay search results */} 
+            {searchResults.map((val)=>(<SearchItem nombre={val.data} link={val.id} key={val.id}/>))}
 
-      {/* Diplay search results */}
-      {searchResults.map((val) => (
-        <SearchItem nombre={val.data} link={val.id} key={val.id} />
-      ))}
-    </div>
-  );
+        </div>        
+    )
 }
 
-export default Searcher;
+export default Searcher
