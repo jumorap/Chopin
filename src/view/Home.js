72f8ba0fe5c1dda95d2444ef93a6774ed3a9a0de
Carlos Jimenez <cajimenezca@unal.edu.co>
2021-05-14 02:35:05 -0500

    filters and indetation

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import UploadFile from './UploadFile/UploadFile'
+import React from "react";
+import NavBar from "./components/NavBar";
+import UploadFile from "./UploadFile/UploadFile";
 
 function Home() {
-    return (
-        <div>
-            <NavBar/>
-            <UploadFile/>
-        </div>
-    )
+  return (
+    <div>
+      <NavBar />
+      <UploadFile />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
