531ecfc62843b657f835a48c2ace660b3d6d865e
jumorap <jumorap@unal.edu.co>
2021-05-14 03:17:50 -0500

    With a firebase function was added the logout option in the component NavBar.js

--- src/view/components/NavBar.js
+++ src/view/components/NavBar.js
@@ -5,27 +5,33 @@ import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons/faSignOutAlt";
 import logo from "../assets/principal-logo.png";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { firebaseAppAuth } from "../../model/firebaseSelf/firebaseConfig";
+
 
 function NavBar() {
-  return (
-    <nav className={"main-navbar"}>
-      <Link to="/">
-        <div className={"logo-container-nav"}>
-          <center>
-            <input className={"logo-image"} type="image" src={logo} alt={""} />
-          </center>
-        </div>
-      </Link>
+    function signOutClick(){
+        return firebaseAppAuth.signOut()
+    }
 
-      <div className={"searcher-container-nav"}>
-        <Searcher />
-      </div>
+    return (
+        <nav className={"main-navbar"}>
+            <Link to="/">
+                <div className={"logo-container-nav"}>
+                    <center>
+                        <input className={"logo-image"} type="image" src={logo} alt={""} />
+                    </center>
+                </div>
+            </Link>
 
-      <div className={"log-out-container-nav"}>
-        <FontAwesomeIcon icon={faSignOutAlt} className={"sign-out"} />
-      </div>
-    </nav>
-  );
+            <div className={"searcher-container-nav"}>
+                <Searcher />
+            </div>
+
+            <div className={"log-out-container-nav"}>
+                <FontAwesomeIcon icon={faSignOutAlt} className={"sign-out"} onClick={signOutClick}/>
+            </div>
+        </nav>
+      );
 }
 
 export default NavBar;
