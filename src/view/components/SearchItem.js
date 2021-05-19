0102bfd3be4e5a1f4d305b74caa28e1385801187
jumorap <jumorap@unal.edu.co>
2021-05-18 22:45:04 -0500

    Used refractor method in UploadForm.js and fixed a bug to redirect pages in SearchItem.js

--- src/view/components/SearchItem.js
+++ src/view/components/SearchItem.js
@@ -4,10 +4,12 @@ import "../css/searchItem.css";
 
 function SearchItem({ nombre, link }) {
   return (
-    <Link to={`/materias/${link}`} className="search-item-link">
-      <div className="search-item-container">{nombre}</div>
-    </Link>
-  );
+      <span>
+          <a href={`/materias/${link}`} className="search-item-link" >
+              <div className="search-item-container">{nombre}</div>
+          </a>
+      </span>
+  )
 }
 
 export default SearchItem;
