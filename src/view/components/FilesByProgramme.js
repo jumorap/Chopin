6fe365e8d620c2f87b983568253b60103a3c9f8d
Carlos Jimenez <cajimenezca@unal.edu.co>
2021-06-03 19:06:55 -0500

    delete modal confirmation finished

--- src/view/components/FilesByProgramme.js
+++ src/view/components/FilesByProgramme.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../css/filesByProgramme.css";
-import { Modal, Tooltip } from "@material-ui/core";
+import { Modal, Tooltip, Popover, CardContent } from "@material-ui/core";
 import {
   FaExternalLinkAlt,
   AiFillCloseCircle,
@@ -11,6 +11,7 @@ import { firebaseAppAuth } from "../../model/firebaseSelf/firebaseConfig";
 export function FilesByProgramme({ items = [] }) {
   const [modal, setModal] = useState(false);
   const [clicked, setClicked] = useState(undefined);
+  const [delModal, setDelModal] = useState(false);
   const currentUserID = firebaseAppAuth.currentUser.uid;
   const openAndClose = (item) => {
     setModal(!modal);
@@ -53,43 +54,100 @@ export function FilesByProgramme({ items = [] }) {
     </>
   );
 
+  let CardContent = ({ item }) => (
+    <div className={"files-programme"} onClick={() => openAndClose(item)}>
+      <div className={"file-by-type"}>{item.tipo}</div>
+      <div className={"file-by-description"}>{item.comentarios}</div>
+      <div className={"file-by-teacher"}>
+        <b>{item.semestre}</b> - {item.profesor}
+      </div>
+      <div className={"like"} />
+    </div>
+  );
+
+  let DeleteModal = ({ item }) => (
+    <Modal
+      disableAutoFocus
+      disableEscapeKeyDown
+      disableBackdropClick
+      onEscapeKeyDown={(e) => console.log(e.target)}
+      open={delModal}
+      onClose={() => setDelModal(false)}
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+    >
+      <div className="delete-modal">
+        <h1>Eliminar!</h1>
+        <span>¿Seguro deseas eliminar el archivo?</span>
+        <div>
+          <button
+            style={{
+              background: "green",
+              border: "none",
+              borderRadius: "5px",
+              color: "white",
+            }}
+            onClick={() => setDelModal(false)}
+          >
+            Cancelar
+          </button>
+          <button
+            style={{
+              background: "red",
+              border: "none",
+              borderRadius: "5px",
+              color: "white",
+            }}
+            onClick={() => {
+              setDelModal(false);
+              deleteFile(item);
+            }}
+          >
+            Eliminar
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
+
+  let CardBtns = ({ item }) => {
+    return (
+      currentUserID == item.usuario && (
+        <>
+          <Tooltip title={"Eliminar archivo"}>
+            <div className={"delete"}>
+              <AiFillCloseCircle
+                onClick={() => setDelModal(true)}
+                className={"delete-component"}
+                style={{ padding: "3px 0px" }}
+              />
+            </div>
+          </Tooltip>
+          <DeleteModal item={item} />
+          <Tooltip title={"Editar archivo"}>
+            <div className={"edit"}>
+              <AiFillEdit
+                onClick={editFile}
+                className={"edit-component"}
+                style={{ padding: "3px 0px" }}
+              />
+            </div>
+          </Tooltip>
+        </>
+      )
+    );
+  };
+
   let cards = items.map(
     (item, index) =>
       item && (
-        <div className={"card-container"} key={index}>
-          <div className={"files-programme"} onClick={() => openAndClose(item)}>
-            <div className={"file-by-type"}>{item.tipo}</div>
-            <div className={"file-by-description"}>{item.comentarios}</div>
-            <div className={"file-by-teacher"}>
-              <b>{item.semestre}</b> - {item.profesor}
-            </div>
-
-            <div className={"like"} />
-
-            {currentUserID == item.usuario && (
-              <>
-                <Tooltip title={"Eliminar archivo"}>
-                  <div className={"delete"}>
-                    <AiFillCloseCircle
-                      onClick={deleteFile}
-                      className={"delete-component"}
-                      style={{ padding: "3px 0px" }}
-                    />
-                  </div>
-                </Tooltip>
-                <Tooltip title={"Editar archivo"}>
-                  <div className={"edit"}>
-                    <AiFillEdit
-                      onClick={editFile}
-                      className={"edit-component"}
-                      style={{ padding: "3px 0px" }}
-                    />
-                  </div>
-                </Tooltip>
-              </>
-            )}
-          </div>
-
+        <div
+          className={"card-container"}
+          key={index}
+          style={{ position: "relative" }}
+        >
+          <CardContent item={item} />
+          <CardBtns item={item} />
           <Modal open={modal} onClose={openAndClose}>
             {body()}
           </Modal>
