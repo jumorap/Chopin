b2dfea51f8ec988de358c4fda17290488c5cb6ef
jumorap <jumorap@unal.edu.co>
2021-06-10 03:02:23 -0500

    Improved the browser accessibility from 84% to 100%, removed unused files. Was found a trouble to upload files in principal view

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Login from "./view/Login";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Admin from './view/Admin';
-import Home from './view/Home';
 import ProgrammeResults from "./view/ProgrammeResults";
 import { createContext, useEffect } from 'react';
 import ContextProvider from './view/ContextProvider';
@@ -27,7 +26,6 @@ function App() {
                     <PrivateRoute exact path="/materias/:idMateria" component={ ProgrammeResults} />
                     <PrivateRoute exact path="/results" component={ProgrammeResults} />
                     <PrivateRoute exact path="/admin" component={Admin} />
-                    <PrivateRoute exact path="/home" component={Home} />
 
                     <Route exact path="/" component={Login} />
                 </ContextProvider>
