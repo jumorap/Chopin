1ad7abdc409937989a37bec3f9d1b438b5c3e9bd
ElSistemologo <leandrogomezfaes@gmail.com>
2021-06-12 20:53:18 -0500

    delete function tested

--- src/controler/MateriasView.js
+++ src/controler/MateriasView.js
@@ -1,83 +1,60 @@
 class MateriasView {
-  constructor() {
-    this.mapMaterias = new Map();
-  }
-
-  /** Delete desired archive from context 
-   * 
-   * @param {Obj} trabajoToDelete  gets the desired trabajo (archive in context- for ex: Parcial 3 ) to delete
-   * @param {Map} currentMateria  gets the current materia of the context 
-   */
-   delete_archivo(trabajoToDelete, currentMateria) {
-    /* get desired materia obj*/
-    let materia = this.mapMaterias.get(currentMateria);
-    /* removes specific trabajo from materia.trabajos */
-    materia.trabajos = materia.trabajos.filter(
-      (trabajo) => trabajo.ID_archivo !== trabajoToDelete.ID_archivo
-    );
-
-    /* Removes trabajo from materia.profesores */
-    let profesores = materia.profesores;
-    this.delete_types(trabajoToDelete, profesores);
-
-    /* Removes trabajo from materia.semestres */
-    let semestres = materia.semestres;
-    this.delete_types(trabajoToDelete, semestres);
-
-    /* Removes trabajo from materia.tipos */
-    let tipos = materia.tipos;
-    this.delete_types(trabajoToDelete, tipos);
-  }
-
-  /** This one executes inside delete_archivo(); - deletes the materia's info which is 
-   * pointed inside an specific materia type 
-   *   
-   * 
-   * @param {Obj} trabajoToDelete  gets the desired trabajo (archive in context- for ex: Parcial 3 ) to delete
-   * @param {Map} materiaType gets the specific materia type to delete 
-   * (this ones goes from the variable "materia" created inside 'delete_archivo' function)
-   */
-  delete_types(trabajoToDelete, materiaType) {
-    Object.keys(materiaType).forEach((type) => {
-      let filesType = materiaType[type];
-      delete filesType[trabajoToDelete.ID_archivo];
-    });
-  }
-
-
-
-
-//   delete(trabajoToDelete, currentMateria) {
-//     /* get desired materia obj*/
-//     let materia = this.mapMaterias.get(currentMateria);
-//     /* removes specific trabajo from materia.trabajos */
-//     materia.trabajos = materia.trabajos.filter(
-//       (trabajo) => trabajo.ID_archivo !== trabajoToDelete.ID_archivo
-//     );
-//     /* Removes trabajo from materia.profesores */
-//     let profesores = materia.profesores;
-//     Object.keys(profesores).forEach((prof) => {
-//       let filesProfe = profesores[prof];
-//       delete filesProfe[trabajoToDelete.ID_archivo];
-//     });
-//     /* Removes trabajo from materia.semestres */
-//     let semestres = materia.semestres;
-//     Object.keys(semestres).forEach((sem) => {
-//       let filesSem = semestres[sem];
-//       delete filesSem[trabajoToDelete.ID_archivo];
-//     });
-//     /* Removes trabajo from materia.tipos */
-//     let tipos = materia.tipos;
-//     Object.keys(tipos).forEach((type) => {
-//       let filesType = tipos[type];
-//       delete filesType[trabajoToDelete.ID_archivo];
-//     });
-//   }
+    constructor() {
+      this.mapMaterias = new Map();
+    }
+  
+    /** Delete desired archive from context
+     *
+     * @param {Obj} trabajoToDelete  gets the desired trabajo (archive in context- for ex: Parcial 3 ) to delete
+     * @param {Obj} currentMateria  gets the current materia of the context
+     */
+     delete_archivo(trabajoToDelete, currentMateria) {
+      /* get desired materia obj*/
+  
+      console.log("INPUT -----");
+      let materia = this.mapMaterias.get(currentMateria);
+      console.log("ANTES  ", materia);
+      /* removes specific trabajo from materia.trabajos */
+      materia.trabajos = materia.trabajos.filter(
+        (trabajo) => trabajo.ID_archivo !== trabajoToDelete.ID_archivo
+      );
+  
+      /* Removes trabajo from materia.profesores */
+      let profesores = materia.profesores;
+      this.delete_types(trabajoToDelete, profesores);
+  
+      /* Removes trabajo from materia.semestres */
+      let semestres = materia.semestres;
+      this.delete_types(trabajoToDelete, semestres);
+  
+      /* Removes trabajo from materia.tipos */
+      let tipos = materia.tipos;
+      this.delete_types(trabajoToDelete, tipos);
+  
+      console.log("DESPUES  ", materia.profesores["Profesor Pato"]);
+    }
+  
+    /** This one executes inside delete_archivo(); - deletes the materia's info which is
+     * pointed inside an specific materia type
+     *
+     *
+     * @param {Obj} trabajoToDelete  gets the desired trabajo (archive in context- for ex: Parcial 3 ) to delete
+     * @param {Map} materiaType gets the specific materia type to delete
+     * (this ones goes from the variable "materia" created inside 'delete_archivo' function)
+     */
+    delete_types(trabajoToDelete, materiaType) {
+      Object.keys(materiaType).forEach((type) => {
+        let filesType = materiaType[type];
+        delete filesType[trabajoToDelete.ID_archivo];
+      });
     
-  update(trabajoToDelete, currentMateria) {
-    this.delete(trabajoToDelete, currentMateria);
-    /* Call Add function  */
+    }
+  
+      
+    update(trabajoToDelete, currentMateria) {
+      this.delete(trabajoToDelete, currentMateria);
+      /* Call Add function  */
+    }
   }
-}
-
-export default MateriasView;
+  
+  export default MateriasView;
