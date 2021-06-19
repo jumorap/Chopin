001ff910b42c5dfa2b4b769caa62f05955152a9f
Johan <jorodriguezal@unal.edu.co>
2021-06-18 22:18:34 -0500

    Add archivo

--- src/controler/MateriasView.js
+++ src/controler/MateriasView.js
@@ -2,7 +2,53 @@ class MateriasView {
     constructor() {
       this.mapMaterias = new Map();
     }
-  
+    /**
+     * Validates existence, creates/uses and adds file id as value on a specific key of materiaContext.
+     * @param {Map} map1 - materiaContext's Map.
+     * @param {String} map2 - file's key.
+     * @param {String} id - file's id.
+     */
+    validate_attribute(map1, map2, id) {
+        if (!(map2 in map1)) {
+        map1[map2] = new Map([[id, 1]]);
+        } else {
+        let ma = map1[map2];
+        ma.set(id, 1);
+        }
+    }
+
+    /**
+     * Adds new file to materiaContext.
+     * @param {Map} materiaMap - materiaContext object in which file is added.
+     * @param {Map} archivoMap - file object that is going to be added.
+     */
+    add_archivo_context(materiaMap, archivoMap) {
+        materiaMap.trabajos.push(archivoMap);
+        let id = archivoMap.id_archivo;
+        this.validate_attribute(materiaMap.profesores, archivoMap.profesores, id);
+        this.validate_attribute(materiaMap.semestres, archivoMap.semestres, id);
+        this.validate_attribute(materiaMap.tipos, archivoMap.tipo, id);
+    }
+
+
+    /** Adds file to context
+     * @param {Obj} archivo  archivo to add to the context
+     */
+    add_archivo(archivo) {
+        //check if the materia exists
+        if (!this.mapMaterias.has(archivo.id_materia)) {
+          console.log("la materia no se encuentra en el map");
+          return;
+        }
+        // get the materia to be modifed
+        const materia_context = this.mapMaterias.get(archivo.id_materia);
+        /*console.log(archivo);
+        console.log(materia_context);*/
+    
+        // modifie the materia
+        this.add_archivo_context(materia_context, archivo);
+      }
+
     /** Delete desired archive from context
      *
      * @param {Obj} trabajoToDelete  gets the desired trabajo (archive in context- for ex: Parcial 3 ) to delete
