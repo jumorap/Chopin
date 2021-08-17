e1803bcfa9eb23c55eca869940f7904efdcd8152
Jahel Leon <jleonro@unal.edu.co>
2021-08-17 12:12:02 -0500

    env added

--- src/model/firebaseSelf/firebaseConfig.js
+++ src/model/firebaseSelf/firebaseConfig.js
@@ -6,15 +6,16 @@ import "firebase/analytics"
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyD8TsjByeLBm65H3RyrJ11gyiucmceI0YE",
-    authDomain: "red-board-70d99.firebaseapp.com",
-    databaseURL: "https://red-board-70d99-default-rtdb.firebaseio.com",
-    projectId: "red-board-70d99",
-    storageBucket: "red-board-70d99.appspot.com",
-    messagingSenderId: "418211670011",
-    appId: "1:418211670011:web:38f96f43a16020a416cd5e",
-    measurementId: "G-TXX9Q3017X"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
 };
+
 
 export let firebaseInitApp;
 
